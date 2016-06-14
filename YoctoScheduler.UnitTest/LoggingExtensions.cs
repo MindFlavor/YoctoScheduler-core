b310c56f486aba0b458a7257500478564d5548ae
Francesco Cogno <francesco.cogno@outlook.com>
2016-06-14 12:25:57 +0200

    Added Trace and Verbose logging extensions

---
+++ YoctoScheduler.UnitTest/LoggingExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YoctoScheduler.Logging.Extensions;
+
+namespace YoctoScheduler.UnitTest
+{
+    [TestClass]
+    public class LoggingExtensions
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(LoggingExtensions));
+
+        [TestMethod]
+        public void LoggingExtensions_Test1()
+        {
+            log.DebugFormat("Time now is {0:S}", DateTime.Now.ToString());
+            log.InfoFormat("Time now is {0:S}", DateTime.Now.ToString());
+            log.WarnFormat("Time now is {0:S}", DateTime.Now.ToString());
+            log.ErrorFormat("Time now is {0:S}", DateTime.Now.ToString());
+            log.FatalFormat("Time now is {0:S}", DateTime.Now.ToString());
+
+            log.TraceFormat("Time now is {0:S}", DateTime.Now.ToString());
+            log.VerboseFormat("Time now is {0:S}", DateTime.Now.ToString());
+        }
+    }
+}
